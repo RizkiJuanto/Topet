d237d9f3bdc676b8445adcc45aa6d8cc3186b6f7
RizkiJuanto <rizki.juanto@binus.ac.id>
2024-05-18 13:13:14 +0700

    feat: initialize project

---
+++ Assets/js/js.js
@@ -0,0 +1,14 @@
+function on(){
+    var menubar = document.getElementById("menuBar")
+
+    var overlay = document.getElementById("overlay")
+
+    menubar.classList.toggle("menuBarStyle")
+    overlay.classList.toggle("overlayStyle")
+}
+
+function kategoriOn(){
+    var category = document.getElementById("category1");
+
+    category.classList.toggle("categoryStyle")
+}
